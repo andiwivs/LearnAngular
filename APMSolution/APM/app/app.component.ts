9acddf55d2fd1a76ec69cbf170ef3cf2292fefb6
Andrew Withers <andiwivs@gmail.com>
2016-09-03 00:12:12 +0100

    Add app component

--- APMSolution/APM/app/app.component.ts
+++ APMSolution/APM/app/app.component.ts
@@ -1,2 +1,14 @@
-﻿export class AppComponent {
+﻿import { Component } from "angular2/core";
+
+@Component({
+    selector: "pm-app",
+    template: `
+        <div>
+            <h1>{{pageTitle}}</h1>
+            <div>My first component</div>
+        </div>
+    `
+})
+export class AppComponent {
+    pageTitle: string = "ACME Product Management";
 }
