438318baa3f21bde6af8339659b278a1ca57e138
Andrew Withers <andiwivs@gmail.com>
2016-09-04 21:49:33 +0100

    Add product-list component script

---
+++ APMSolution/APM/app/products/product-list.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from "angular2/core";
+
+@Component({
+    selector: "pm-products",
+    templateUrl: "app/products/product-list.component.html"
+})
+export class ProductListComponent {
+
+}
